e3d5c1854cf59d9d56974282720cc724c7b73500
Farkhiev, Azamat <azamat.farkhiev@wartsila.com>
2020-12-13 21:45:22 +0300

    minimal working version

--- TodoList/ContentView.swift
+++ TodoList/ContentView.swift
@@ -1,12 +1,48 @@
 import SwiftUI
 
+struct TodoCellView: View {
+    @StateObject var task: Task
+
+    var body: some View {
+        HStack(alignment: .center) {
+            Button(action: { task.toggle() }) {
+                HStack(alignment: .top) {
+                    Image(systemName: task.done ? "checkmark.circle.fill" : "circle").colorScheme(.light)
+                    let color: Color = task.done ? .gray : .black
+                    Text(task.name)
+                        .foregroundColor(color)
+                        .strikethrough(task.done, color: .green)
+                    Text(String(task.priority))
+                        .foregroundColor(color)
+                }
+            }.buttonStyle(BorderlessButtonStyle())
+            Button(action: { task.increasePriority() }) {
+                Image(systemName: "arrowtriangle.up.fill")
+            }.buttonStyle(BorderlessButtonStyle())
+            Button(action: { task.decreasePriority() }) {
+                Image(systemName: "arrowtriangle.down.fill")
+            }.buttonStyle(BorderlessButtonStyle())
+        }
+    }
+}
+
 struct TodoListView: View {
     @StateObject var viewModel = TodoListViewModel()
-    
+
     var body: some View {
-        
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                TextField("Task name", text: $viewModel.name, onEditingChanged: { _ in }, onCommit: {
+                    viewModel.addTask()
+                })
+                Button(action: { viewModel.addTask() }) {
+                    Image(systemName: "plus.circle")
+                }.disabled(!viewModel.addTaskIsEnabled)
+            }
+            List(viewModel.tasks) { task in
+                TodoCellView(task: task)
+            }
+        }
     }
 }
 
@@ -15,5 +51,3 @@ struct ContentView_Previews: PreviewProvider {
         TodoListView()
     }
 }
-
-Identifiable
--- TodoList/TodoListViewModel.swift
+++ TodoList/TodoListViewModel.swift
@@ -1,5 +1,78 @@
 import SwiftUI
 
+protocol TaskDelegate {
+    func taskIsUpdated()
+}
+
+class Task: Identifiable, ObservableObject {
+    private static let MAX_PRIORITY = 10
+    private static let MIN_PRIORITY = 1
+
+    private let delegate: TaskDelegate
+
+    @Published private(set) var done: Bool = false {
+        didSet {
+            if oldValue != done {
+                delegate.taskIsUpdated()
+            }
+        }
+    }
+    @Published private(set) var priority = Task.MIN_PRIORITY {
+        didSet {
+            if oldValue != priority {
+                delegate.taskIsUpdated()
+            }
+        }
+    }
+
+    var id = UUID()
+    var name: String
+
+    init(name: String, delegate: TaskDelegate) {
+        self.name = name
+        self.delegate = delegate
+    }
+
+    func toggle() {
+        done.toggle()
+    }
+
+    func increasePriority() {
+        priority = min(priority + 1, Task.MAX_PRIORITY)
+    }
+
+    func decreasePriority() {
+        priority = max(priority - 1, Task.MIN_PRIORITY)
+    }
+}
+
 class TodoListViewModel: ObservableObject {
-    
+    @Published private(set) var tasks = [Task]()
+    @Published var name = "" {
+        didSet {
+            addTaskIsEnabled = !name.isEmpty
+        }
+    }
+    @Published var addTaskIsEnabled: Bool = false
+
+    private func sortTasks() {
+        withAnimation {
+            tasks.sort(by: { !$0.done && $1.done ||
+                    ($0.done == $1.done && $0.priority > $1.priority) ||
+                    ($0.done == $1.done && $0.priority == $1.priority && $0.name < $1.name)
+            })
+        }
+    }
+
+    func addTask() {
+        tasks.append(Task(name: name, delegate: self))
+        sortTasks()
+        name = ""
+    }
+}
+
+extension TodoListViewModel: TaskDelegate {
+    func taskIsUpdated() {
+        sortTasks()
+    }
 }
